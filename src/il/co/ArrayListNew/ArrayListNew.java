cfcd794aa04f3c4aefe8ce59e029876b60227b96
artmeleshko <artem.meleshko@gmail.com>
2016-08-15 21:25:45 +0300

    I've deleted the Main method from ArrayListNew.java.

--- src/il/co/ArrayListNew/ArrayListNew.java
+++ src/il/co/ArrayListNew/ArrayListNew.java
@@ -96,49 +96,7 @@ public class ArrayListNew implements Container {
 		 return new NameIterator();
 	}
 	
-	public static void main(String[] args) {
-		ArrayListNew mass1 = new ArrayListNew(5);
-		System.out.println(mass1);
-		mass1.add("Hello World(0)!");
 		
-		mass1.set(0, "Hey guys!(0)");
-		mass1.add("Hello  World!(1)");
-		mass1.add("Hello Sport!()2");
-		mass1.add("Hello++ World!(3)");
-		mass1.add("Hello Sport!()4");
-		System.out.println(mass1.get(2));
-		System.out.println(mass1.size());
-		
-		//mass1.remove(0);
-		System.out.println(mass1.get(1));
-		System.out.println(mass1.size());
-		
-		System.out.println(  mass1.lenght() );
-		mass1.add(2, "Hello Bentley()3!");
-		System.out.println(  mass1.lenght() );
-		System.out.println(mass1.get(0));
-		System.out.println(mass1.get(1));
-		System.out.println(mass1.get(2));
-		System.out.println(mass1.get(3));
-		System.out.println(mass1.get(4));
-		mass1.add("@!@!@!@!@!@!@!@!()5");
-		System.out.println(mass1.get(5));
-		System.out.println(mass1.size());
-		mass1.remove(2);
-		
-		System.out.println(mass1.size()+" Last");
-		
-		System.out.println(  mass1.lenght() );
-		
-		for(Iterator iter = mass1.getIterator(); iter.hasNext();){
-	         String name = (String)iter.next();
-	         System.out.println("Name : " + name);
-	      } 	
-		
-	}
-
-	
-	
 	private class NameIterator implements Iterator {
 
 	      int index;
